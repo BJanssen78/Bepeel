aff4eff9a5ab185f0fa65e3d03d9c5c11bae9774
Ben Janssen <98946596+BJanssen78@users.noreply.github.com>
2022-12-10 14:29:45 +0100

    updated resource info

--- script/footer.js
+++ script/footer.js
@@ -1,26 +1,40 @@
-const footerContent = document.getElementById('footer');
+const footerContent = document.querySelector('#footer');
 
 // Declare element creation
-let createUl = document.createElement('ul');
-let createLi = document.createElement('li');
-let createA = document.createElement('a');
-let createImg = document.createElement('img');
+// let createUl = document.createElement('ul');
+// let createLi = document.createElement('li');
+// let createA = document.createElement('a');
+// let createImg = document.createElement('img');
 let createH2 = document.createElement('h2');
+// let createSpan = document.createElement('span');
+// let createDiv = document.createElement('div');
 
 // Declare arrays.
 let lijstVanNamen = resources.map(namen => namen.naam);
 
 footerContent.appendChild(createH2);
-createH2.innerHTML = 'Resources:';
-footerContent.appendChild(createUl);
+createH2.innerHTML = 'Resources';
 
-// Create basic footer content.
-let basicFooter = resources.forEach(function(e){
-    let liList = '';
+let basicFooter2 = resources.forEach(function(e){
 
-    for(let i=0; i < lijstVanNamen.length; i++){
-        liList += '<li class="footer-resource-item">' + lijstVanNamen[i] + '</li>';
-        document.getElementById('footer').innerHTML = liList;
-    }
+    let createA = document.createElement('a');
+    let createImg = document.createElement('img');
+
+    let createSpan = document.createElement('span');
     
+    // Create link
+    footerContent.appendChild(createA);
+    createA.setAttribute('class', 'resource-link');
+    createA.setAttribute('href', e.website);
+    createA.setAttribute('target', '_blank');
+    
+    createA.appendChild(createImg);
+    createImg.setAttribute('class', 'resource-logo');
+    createImg.setAttribute('src', e.externLogo);
+    createImg.setAttribute('alt', e.naam);
+    createImg.setAttribute('title', e.naam);
+
+    createA.appendChild(createSpan);
+    createSpan.setAttribute('class', 'resource-text');
+    createSpan.innerHTML = e.naam;
 });
--- script/resources.js
+++ script/resources.js
@@ -5,30 +5,30 @@ const resources = [
         kanaal: "YouTube",
         naam: "Kevin Powell",
         website: "https://www.youtube.com/@KevinPowell/featured",
-        externLogo: imgLink + "logo_youtube.jpg"
+        externLogo: imgLink + "logo_youtube.jpg",
     },
     {
         kanaal: "website",
         naam: "w3schools",
         website: "https://www.w3schools.com/",
-        externLogo: imgLink + "W3Schools_logo.svg"
+        externLogo: imgLink + "logo_website.png",
     },
     {
         kanaal: "YouTube",
         naam: "The Net Ninja",
         website: "https://www.youtube.com/@NetNinja",
-        externLogo: imgLink + "logo_youtube.jpg"
+        externLogo: imgLink + "logo_youtube.jpg",
     },
     {
         kanaal: "YouTube",
         naam: "Mike Dane",
         website: "https://www.youtube.com/@GiraffeAcademy",
-        externLogo: imgLink + "logo_youtube.jpg"
+        externLogo: imgLink + "logo_youtube.jpg",
     },
     {
         kanaal: "website",
         naam: "Winc Academy",
         website: "https://www.wincacademy.nl/#",
-        externLogo: imgLink + "logo_website.png"
+        externLogo: imgLink + "logo_website.png",
     },
 ]
