016d4268a269060d3abdd088d0e1bab03407eccf
Ben Janssen <98946596+BJanssen78@users.noreply.github.com>
2022-12-11 14:58:33 +0100

    updated footer

--- script/footer.js
+++ script/footer.js
@@ -1,40 +1,120 @@
 const footerContent = document.querySelector('#footer');
 
 // Declare element creation
-// let createUl = document.createElement('ul');
-// let createLi = document.createElement('li');
-// let createA = document.createElement('a');
-// let createImg = document.createElement('img');
+let createUl = document.createElement('ul');
 let createH2 = document.createElement('h2');
-// let createSpan = document.createElement('span');
-// let createDiv = document.createElement('div');
-
-// Declare arrays.
-let lijstVanNamen = resources.map(namen => namen.naam);
-
-footerContent.appendChild(createH2);
+footerContent.appendChild(createUl);
+createUl.setAttribute('id', 'resources');
+createUl.appendChild(createH2);
 createH2.innerHTML = 'Resources';
 
-let basicFooter2 = resources.forEach(function(e){
+let createUl2 = document.createElement('ul');
+let createH22 = document.createElement('h2');
+footerContent.appendChild(createUl2);
+createUl2.setAttribute('id', 'wikis');
+createUl2.appendChild(createH22);
+createH22.innerHTML = 'Wikis / Libaries';
 
-    let createA = document.createElement('a');
-    let createImg = document.createElement('img');
+let createUl3 = document.createElement('ul');
+let createH23 = document.createElement('h2');
+footerContent.appendChild(createUl3);
+createUl3.setAttribute('id', 'programs');
+createUl3.appendChild(createH23);
+createH23.innerHTML = 'Programs';
 
-    let createSpan = document.createElement('span');
-    
-    // Create link
-    footerContent.appendChild(createA);
-    createA.setAttribute('class', 'resource-link');
-    createA.setAttribute('href', e.website);
-    createA.setAttribute('target', '_blank');
-    
-    createA.appendChild(createImg);
-    createImg.setAttribute('class', 'resource-logo');
-    createImg.setAttribute('src', e.externLogo);
-    createImg.setAttribute('alt', e.naam);
-    createImg.setAttribute('title', e.naam);
+let createUl4 = document.createElement('ul');
+let createH24 = document.createElement('h2');
+footerContent.appendChild(createUl4);
+createUl4.setAttribute('id', 'Languages');
+createUl4.appendChild(createH24);
+createH24.innerHTML = 'Languages';
 
-    createA.appendChild(createSpan);
-    createSpan.setAttribute('class', 'resource-text');
-    createSpan.innerHTML = e.naam;
-});
+// Create list content
+let footerResources = resources.filter(kanaal => kanaal.kanaal === 'YouTube')
+    .forEach(function(e){
+        let createLi = document.createElement('li');
+        let createA = document.createElement('a');
+        let createI = document.createElement('i');
+        let createImg = document.createElement('img');
+        let createSpan = document.createElement('span');
+
+        createUl.appendChild(createLi);
+        createLi.appendChild(createA);
+        createA.appendChild(createI);
+
+        createA.setAttribute('href', e.website);
+        createA.setAttribute('target', '_blank');
+
+        createI.setAttribute('class', 'fa-brands fa-youtube');
+
+        createA.appendChild(createSpan);
+        createSpan.setAttribute('class', 'resource-text');
+        createSpan.innerHTML = e.naam;
+    }
+);
+
+let footerWikis = resources.filter(kanaal => kanaal.kanaal === 'website')
+    .forEach(function(e){
+        let createLi = document.createElement('li');
+        let createA = document.createElement('a');
+        let createI = document.createElement('i');
+        let createSpan = document.createElement('span');
+
+        createUl2.appendChild(createLi);
+        createLi.appendChild(createA);
+        createA.appendChild(createI);
+
+        createA.setAttribute('href', e.website);
+        createA.setAttribute('target', '_blank')
+
+        createI.setAttribute('class', 'fa-solid fa-globe');
+
+        createA.appendChild(createSpan);
+        createSpan.setAttribute('class', 'resource-text');
+        createSpan.innerHTML = e.naam;
+    }
+);
+
+let footerPrograms = resources.filter(kanaal => kanaal.kanaal === 'programs')
+    .forEach(function(e){
+        let createLi = document.createElement('li');
+        let createA = document.createElement('a');
+        let createI = document.createElement('i');
+        let createSpan = document.createElement('span');
+
+        createUl3.appendChild(createLi);
+        createLi.appendChild(createA);
+        createA.appendChild(createI);
+
+        createA.setAttribute('href', e.website);
+        createA.setAttribute('target', '_blank');
+
+        createI.setAttribute('class', 'fa-sharp fa-solid fa-floppy-disk');
+
+        createA.appendChild(createSpan);
+        createSpan.setAttribute('class', 'resource-text');
+        createSpan.innerHTML = e.naam;
+    }
+);
+
+let footerLanguage = resources.filter(kanaal => kanaal.kanaal === 'language')
+    .forEach(function(e){
+        let createLi = document.createElement('li');
+        let createA = document.createElement('a');
+        let createI = document.createElement('i');
+        let createSpan = document.createElement('span');
+
+        createUl4.appendChild(createLi);
+        createLi.appendChild(createA);
+        createA.appendChild(createI);
+
+        createA.setAttribute('href', e.website);
+        createA.setAttribute('target', '_blank');
+
+        createI.setAttribute('class', 'fa-solid fa-earth-americas');
+
+        createA.appendChild(createSpan);
+        createSpan.setAttribute('class', 'resource-text');
+        createSpan.innerHTML = e.naam;
+    }
+);
--- script/resources.js
+++ script/resources.js
@@ -31,4 +31,52 @@ const resources = [
         website: "https://www.wincacademy.nl/#",
         externLogo: imgLink + "logo_website.png",
     },
+    {
+        kanaal: "programs",
+        naam: "Visual Studio Code",
+        website: "https://code.visualstudio.com/",
+        externLogo: imgLink + "logo_diskette.png",
+    },
+    {
+        kanaal: "programs",
+        naam: "Visual Studio 2022",
+        website: "https://visualstudio.microsoft.com/vs/",
+        externLogo: imgLink + "logo_diskette.png",
+    },
+    {
+        kanaal: "programs",
+        naam: "Node.js",
+        website: "https://nodejs.org/en/",
+        externLogo: imgLink + "logo_diskette.png",
+    },
+    {
+        kanaal: "website",
+        naam: "Font Awesome",
+        website: "https://fontawesome.com/",
+        externLogo: imgLink + "logo_website.png",
+    },
+    {
+        kanaal: "language",
+        naam: "HTML",
+        website: "https://www.w3schools.com/",
+        externLogo: imgLink + "logo_language.jpg",
+    },
+    {
+        kanaal: "language",
+        naam: "CSS / SCSS / SASS",
+        website: "https://www.w3schools.com/",
+        externLogo: imgLink + "logo_language.jpg",
+    },
+    {
+        kanaal: "language",
+        naam: "Javascript",
+        website: "https://www.w3schools.com/",
+        externLogo: imgLink + "logo_language.jpg",
+    },
+    {
+        kanaal: "language",
+        naam: "Python",
+        website: "https://www.w3schools.com/",
+        externLogo: imgLink + "logo_language.jpg",
+    },
 ]
