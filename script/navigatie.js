22b23f7c07ff6fb95caf74a37e16b7c7397c5eb9
Ben Janssen <98946596+BJanssen78@users.noreply.github.com>
2022-12-12 21:17:32 +0100

    added java navi

---
+++ script/navigatie.js
@@ -0,0 +1,20 @@
+const naviElement = document.querySelector('#navigatie');
+
+let createNaviUl = document.createElement('ul');
+naviElement.appendChild(createNaviUl);
+createNaviUl.setAttribute('id', 'nav-list');
+createNaviUl.setAttribute('class', 'navigatie-lijst');
+
+const navigatieLijst = navigatie.forEach(function(e){
+    let createNaviLi = document.createElement('li');
+    let createNaviA = document.createElement('a');
+
+    createNaviUl.appendChild(createNaviLi);
+    createNaviLi.setAttribute('class', 'navigatie-item');
+    createNaviLi.appendChild(createNaviA);
+
+    createNaviA.setAttribute('href', e.link);
+    createNaviA.setAttribute('target', e.target);
+    createNaviA.setAttribute('class', 'navigatie-link');
+    createNaviA.innerHTML = e.titel
+})
--- script/resources.js
+++ script/resources.js
@@ -1,82 +1,107 @@
-const imgLink = "./../img/";
+const root = "./../";
+const imgMap = "img/";
+const pageMap = "pages/";
 
 const resources = [
     {
         kanaal: "YouTube",
         naam: "Kevin Powell",
         website: "https://www.youtube.com/@KevinPowell/featured",
-        externLogo: imgLink + "logo_youtube.jpg",
+        externLogo: root + imgMap + "logo_youtube.jpg",
     },
     {
         kanaal: "website",
         naam: "w3schools",
         website: "https://www.w3schools.com/",
-        externLogo: imgLink + "logo_website.png",
+        externLogo: root + imgMap + "logo_website.png",
     },
     {
         kanaal: "YouTube",
         naam: "The Net Ninja",
         website: "https://www.youtube.com/@NetNinja",
-        externLogo: imgLink + "logo_youtube.jpg",
+        externLogo: root + imgMap + "logo_youtube.jpg",
     },
     {
         kanaal: "YouTube",
         naam: "Mike Dane",
         website: "https://www.youtube.com/@GiraffeAcademy",
-        externLogo: imgLink + "logo_youtube.jpg",
+        externLogo: root + imgMap + "logo_youtube.jpg",
     },
     {
         kanaal: "website",
         naam: "Winc Academy",
         website: "https://www.wincacademy.nl/#",
-        externLogo: imgLink + "logo_website.png",
+        externLogo: root + imgMap + "logo_website.png",
     },
     {
         kanaal: "programs",
         naam: "Visual Studio Code",
         website: "https://code.visualstudio.com/",
-        externLogo: imgLink + "logo_diskette.png",
+        externLogo: root + imgMap + "logo_diskette.png",
     },
     {
         kanaal: "programs",
         naam: "Visual Studio 2022",
         website: "https://visualstudio.microsoft.com/vs/",
-        externLogo: imgLink + "logo_diskette.png",
+        externLogo: root + imgMap + "logo_diskette.png",
     },
     {
         kanaal: "programs",
         naam: "Node.js",
         website: "https://nodejs.org/en/",
-        externLogo: imgLink + "logo_diskette.png",
+        externLogo: root + imgMap + "logo_diskette.png",
     },
     {
         kanaal: "website",
         naam: "Font Awesome",
         website: "https://fontawesome.com/",
-        externLogo: imgLink + "logo_website.png",
+        externLogo: root + imgMap + "logo_website.png",
     },
     {
         kanaal: "language",
         naam: "HTML",
         website: "https://www.w3schools.com/",
-        externLogo: imgLink + "logo_language.jpg",
+        externLogo: root + imgMap + "logo_language.jpg",
     },
     {
         kanaal: "language",
         naam: "CSS / SCSS / SASS",
         website: "https://www.w3schools.com/",
-        externLogo: imgLink + "logo_language.jpg",
+        externLogo: root + imgMap + "logo_language.jpg",
     },
     {
         kanaal: "language",
         naam: "Javascript",
         website: "https://www.w3schools.com/",
-        externLogo: imgLink + "logo_language.jpg",
+        externLogo: root + imgMap + "logo_language.jpg",
     },
     {
         kanaal: "language",
         naam: "Python",
         website: "https://www.w3schools.com/",
-        externLogo: imgLink + "logo_language.jpg",
+        externLogo: root + imgMap + "logo_language.jpg",
     },
-]
+];
+
+const navigatie = [
+    {
+        titel: 'Home',
+        link: 'www.bepeel.nl',
+        target: '_self',
+    },
+    {
+        titel: 'Projects',
+        link: 'projects.html',
+        target: '_self',
+    },
+    {
+        titel: 'About Me',
+        link: 'projects.html',
+        target: '_self',
+    },
+    {
+        titel: 'Contact',
+        link: root + pageMap + 'projects.html',
+        target: '_self',
+    },
+];
